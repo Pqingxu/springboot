a8f895424ac36d12a5f1f46e14d3c44a4a70ce31
Panxu <932633953@qq.com>
2022-04-16 22:54:49 +0800

    基本环境搭建

---
+++ springboot/mybatisplus/src/main/java/com/pan/Application.java
@@ -0,0 +1,15 @@
+package com.pan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author qingxu.pan@tophant.com
+ * @Date 2022/4/16
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ springboot/mybatisplus/src/main/java/com/pan/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.pan.controller;
+
+import org.apache.ibatis.annotations.ResultType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author qingxu.pan@tophant.com
+ * @Date 2022/4/16
+ */
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    @GetMapping()
+    public String hello(){
+        return "hello";
+    }
+
+}
